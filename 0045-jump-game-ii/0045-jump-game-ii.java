d081211c8927874323406ca23e71dffff8f0d712
Neham Joe Baiju <nehamjoe11@gmail.com>
2025-10-28 21:47:43 +0530

    Time: 1 ms (99.73%), Space: 45.6 MB (17.66%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jump=0, currMax=0; 
+        int currEnd=0;
+
+        for (int i=0;i<nums.length-1;i++){
+            currMax = Math.max(currMax, i+nums[i]);
+            if(i==currEnd){
+                jump++;
+                currEnd=currMax;
+            }
+        }
+        return jump;
+    }
+}
