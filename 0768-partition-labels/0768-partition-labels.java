34342b426727b7f72f2f8403248ba64510326463
Neham Joe Baiju <nehamjoe11@gmail.com>
2025-10-29 20:40:49 +0530

    Time: 5 ms (78.33%), Space: 42.4 MB (43.72%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int[] lastOccurrence = new int[26];
+        for(int i=0;i<s.length();i++){
+            lastOccurrence[s.charAt(i) - 'a'] = i;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int partitionEnd = 0;
+        int partitionStart = 0;
+
+        for(int i=0;i< s.length();i++){
+            partitionEnd = Math.max(partitionEnd, lastOccurrence[s.charAt(i)-'a']);
+
+            if(i == partitionEnd){
+                result.add(partitionEnd - partitionStart+1);
+                partitionStart = i+1;
+            }
+        }
+        return result;
+    }
+}
