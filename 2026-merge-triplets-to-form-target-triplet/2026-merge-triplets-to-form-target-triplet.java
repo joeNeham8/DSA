6c65e2931ee4c8159ec1d1aa33e18676177e0a76
Neham Joe Baiju <nehamjoe11@gmail.com>
2025-10-30 21:43:36 +0530

    Time: 6 ms (43.48%), Space: 121 MB (57.43%) - LeetHub

---
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        int[] maxValues = new int[3];
+
+        for(int[] triplet : triplets){
+            if(triplet[0] <=target[0] && triplet[1] <= target[1] && triplet[2] <= target[2]){
+                maxValues[0] = Math.max(maxValues[0], triplet[0]);
+                maxValues[1] = Math.max(maxValues[1], triplet[1]);
+                maxValues[2] = Math.max(maxValues[2], triplet[2]);
+            }
+        }
+        return maxValues[0] == target[0] && maxValues[1] == target[1] && maxValues[2] == target[2];
+    }
+}
